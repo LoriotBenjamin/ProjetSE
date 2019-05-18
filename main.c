1f20ef07523bb8371ce790b9167fc49d219fca45
Benjamin Loriot <benjamin.loriot.pro@gmail.com>
2019-05-18 14:38:54 +0200

    initialisation et remise a zero des arguments à chaque commande

--- main.c
+++ main.c
@@ -4,30 +4,39 @@
 #include <string.h>
 
 
-void myputchar(char* c){
+struct infoinode
+{
+	char permissions[8]; // rwxr--r--
+	int typefichier; // 1 = texte, 2 = binaire , ...
+	int blocutilise[30]; // quels sont les blocs utilisés (max 30 ici)
+	// peut être mettre la longueur du fichier
+};
 
-	write(1,c,1);
-
-}
+struct unbloc {
+	char donnees[1024]; // par exemple 1024 octets 
+};
+struct disk {
+	struct infoinode inode[15]; // 15 inodes
+	struct unbloc bloc[30]; // 30 blocs de 1024 octets 
+};
 
 
 int main (){
 
-	printf("hello world\n");
-
-	char s[100]=" ";
 
 	while(1){
 
+		printf("%d",sizeof(unbloc));
+
 		printf("user@user ");
 
-		char nomCommande[40];
+		char nomCommande[40]="\0";
 
 		char seperator;
 
-		char arg1[40];
+		char arg1[40]="\0";
 
-		char arg2[40];
+		char arg2[40]="\0";
 
 		char buffer[100];
 
@@ -39,9 +48,11 @@ int main (){
 		printf("arg1: %s\n",arg1 );
 		printf("arg2: %s\n",arg2 );
 
+		// traitement de la commande 
+		
+
 
 
 	}
-	//free(s);
 	return 0; 
 }
